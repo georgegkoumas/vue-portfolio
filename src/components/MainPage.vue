d52be9554b9fb74796950213d50c1f9b9e8da1db
George Gkoumas <agoriani@gmail.com>
2023-10-07 00:12:43 +1100

    First uncomplete project commit

---
+++ src/components/MainPage.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="main-container">
+        <div class="container-one">
+            <div class="profile">
+                <img src="../assets/profilepic.png" alt="">"
+                <h3>Georgios Gkoumas</h3>
+                <p>Network and Systems Administrator</p>
+                <p>Developer for fun projects</p>
+            </div>
+            <div class="social-links">
+                <a href="">
+                    <div class="link">
+                        <img src="../assets/github.png" alt="">
+                         <h2>GitHub</h2>
+                     </div>
+                     <div class="link">
+                        <img src="../assets/linkedin.png" alt="">
+                         <h2>LinkedIn</h2>
+                     </div>
+                     <div class="link">
+                        <img src="../assets/twitter.png" alt="">
+                         <h2>X</h2>
+                     </div>
+                    </div>
+                </a>    
+            </div>
+        </div>        
+    </div>       
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+<style scoped>
+
+</style>
